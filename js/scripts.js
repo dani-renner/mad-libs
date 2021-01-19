d97c2ea70cde3c0dac136b7db85ebdb809797595
Dani Renner <danijrenner@gmail.com>
2021-01-19 11:52:22 -0800

    use an array to store users' answers, and use forEach to display the story
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
-    const animalInput = $("input#animal").val();
+    const animalInput= $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
+    const responsesArray = [[".person1", person1Input],[".person2", person2Input],[".animal", animalInput], [".exclamation", exclamationInput],[".verb", verbInput],[".noun", nounInput]];
+    // use forEach WITH jQuery to push the user-input strings into the appropriate span class in the result story
+    responsesArray.forEach(function(responsePair) {
+      $(responsePair[0]).text(responsePair[1]);
+    })
     $("#story").show();
-
-    event.preventDefault();
   });
 });
